7c10dd88965040d8887a2376ad3dc5ad1834a1ee
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-07-16 18:14:56 +0900

    [Feat] #3 CompleteTestNaming.swift > CompleteViewController.swift 로 변경 및 CompleteViewController 클래스 설정, Storyboard에 연결

--- Bingha/Bingha/Views/Home/Complete/CompleteNamingTest.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CompleteNamingTest.swift
-//  Bingha
-//
-//  Created by 이재웅 on 2022/07/15.
-//
-
-import Foundation
---
+++ Bingha/Bingha/Views/Home/Complete/CompleteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CompleteViewController.swift
+//  Bingha
+//
+//  Created by 이재웅 on 2022/07/15.
+//
+
+import UIKit
+
+class CompleteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
