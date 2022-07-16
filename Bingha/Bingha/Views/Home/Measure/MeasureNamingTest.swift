a12ec9db8e4978213c99ed168e19f2e93e9cd9fb
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-07-16 18:16:53 +0900

    [Feat] #3 MeasureNamingTest.swift > MeasureViewController.swift 로 변경 및 MeasureViewController 클래스 설정, Storyboard에 연결

--- Bingha/Bingha/Views/Home/Measure/MeasureNamingTest.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MeasureNamingTest.swift
-//  Bingha
-//
-//  Created by 이재웅 on 2022/07/15.
-//
-
-import Foundation
---
+++ Bingha/Bingha/Views/Home/Measure/MeasureViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MeasureViewController.swift
+//  Bingha
+//
+//  Created by 이재웅 on 2022/07/15.
+//
+
+import UIKit
+
+class MeasureViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
