10b0ef3ef82de47379c87e33c7583c71fb31bba4
seuoh22 <70955060+ohhseungyeon@users.noreply.github.com>
2022-07-19 21:01:36 +0900

    [Feat] #6 측정시작화면 레이아웃 제작

--- Bingha/Bingha/Views/Home/Measure/MeasureViewController.swift
+++ Bingha/Bingha/Views/Home/Measure/MeasureViewController.swift
@@ -9,7 +9,39 @@ import UIKit
 
 class MeasureViewController: UIViewController {
     
+    @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var WalkingDistanceView: UIView!
+    @IBOutlet weak var CarbonReductionView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ViewCustom()
+        
+    }
+    
+    
+    private func ViewCustom() {
+        
+    //WalkingDistanceView Custom
+        WalkingDistanceView.layer.shadowOpacity = 0.2
+        WalkingDistanceView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        WalkingDistanceView.layer.shadowColor = UIColor.darkGray.cgColor
+        WalkingDistanceView.layer.cornerRadius = 20
+        
+    //CarbonReductionView Custom
+        CarbonReductionView.layer.shadowOpacity = 0.2
+        CarbonReductionView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        CarbonReductionView.layer.shadowColor = UIColor.darkGray.cgColor
+        CarbonReductionView.layer.cornerRadius = 20
+        
+    //StartButton Custom
+        StartButton.layer.cornerRadius = StartButton.frame.width / 2
+        StartButton.layer.masksToBounds = true
+        StartButton.layer.shadowOpacity = 0.2
+        StartButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        StartButton.layer.shadowColor = UIColor.darkGray.cgColor
+        StartButton.titleLabel?.font = UIFont.systemFont(ofSize: 32.0, weight: .bold)
     }
 }
