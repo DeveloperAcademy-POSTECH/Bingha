255dec94debfa2155d2e149283aeb90ce1f1f437
Terry-Koo <xpflxhfl94@naver.com>
2022-07-19 15:33:38 +0900

    [Feat] #14 빙하UI 스토리보드 파일 분기

---
+++ Bingha/Bingha/Views/Iceberg/IcebergViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IcebergViewController.swift
+//  Bingha
+//
+//  Created by 이재웅 on 2022/07/15.
+//
+
+import UIKit
+
+class IcebergViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
