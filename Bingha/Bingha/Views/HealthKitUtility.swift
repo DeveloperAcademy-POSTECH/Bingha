3339bf97a8b55b39e74b5a5c5d548c73a256df69
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-07-19 17:47:31 +0900

    [Feat] #11 건강데이터의 걷기달리기 이동거리 읽기 getDistanceWalkingRunning 함수 구현

--- Bingha/Bingha/Views/HealthKitUtility.swift
+++ Bingha/Bingha/Views/HealthKitUtility.swift
@@ -24,3 +24,41 @@ func requestHealthKitAuthorization() {
         }
     }
 }
+
+func getDistanceWalkingRunning(startDate: Date, completion: @escaping (Double) -> (Void)) {
+    func convertMileToKM(_ mile: Double) -> Double {
+        mile * 1.60934
+    }
+    
+    if HKHealthStore.isHealthDataAvailable() {
+        requestHealthKitAuthorization()
+        
+        guard let quantityType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning) else { return }
+        
+        let now = Date()
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: quantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, error in
+            var distance: Double = 0.0
+            
+            guard let result = result else {
+                debugPrint("HealthKit Query 생성 실패")
+                return
+            }
+            guard let sum = result.sumQuantity() else {
+                debugPrint("HealthKit Query 결과값 합산 실패")
+                return
+            }
+            
+            distance = sum.doubleValue(for: HKUnit.mile())
+            
+            DispatchQueue.main.async {
+                completion(convertMileToKM(distance))
+            }
+        }
+        
+        healthStore?.execute(query)
+    } else {
+        debugPrint("HealthKit 데이터 사용불가")
+    }
+}
