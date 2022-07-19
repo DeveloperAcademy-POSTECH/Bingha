e927f68caa7b1016252f6b19d79bf2a92449543b
Terry-Koo <xpflxhfl94@naver.com>
2022-07-20 03:25:03 +0900

    [Feat] #14 빙하 UI구현, 경험치 보여줄 CircularPregressBarView UI 구현

--- Bingha/Bingha/Views/Iceberg/IcebergViewController.swift
+++ Bingha/Bingha/Views/Iceberg/IcebergViewController.swift
@@ -8,8 +8,34 @@
 import UIKit
 
 class IcebergViewController: UIViewController {
+    @IBOutlet var roundedRectangle: UIView!
+    @IBOutlet var levelLabel: UILabel!
+    
+    var circularProgressBarView: CircularProgressBarView!
+    var circularViewDuration: TimeInterval = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundedRectangle.layer.cornerRadius = 20
+        setUpCircularProgressBarView()
+        setLevel()
     }
+    
+    // TODO: - 경험치에 따라 levelLabel 동적으로 변하게
+    func setLevel() {
+        levelLabel.text = "Lv.1"
+    }
+    
+    func setUpCircularProgressBarView() {
+        circularProgressBarView = CircularProgressBarView(frame: .zero)
+        // TODO: - 경험치에 맞게 endPoint 파라미터 넣어주기
+        circularProgressBarView.createCircularPath(endPoint: CGFloat(2 * Double.pi / 2))
+        // TODO: - center로 맞췄는데도 불구하고 중심이 안맞음 추후에 수정 필요
+        circularProgressBarView.center = levelLabel.center
+        circularProgressBarView.progressAnimation(duration: circularViewDuration)
+        view.addSubview(circularProgressBarView)
+   }
 }
+
+
+
