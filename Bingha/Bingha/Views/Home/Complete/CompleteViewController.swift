fd220beadb87ff2763e3a778118037eab91c2a9c
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-07-20 15:17:17 +0900

    [Feat] #10 결과 화면 레이아웃 수정

--- Bingha/Bingha/Views/Home/Complete/CompleteViewController.swift
+++ Bingha/Bingha/Views/Home/Complete/CompleteViewController.swift
@@ -12,31 +12,42 @@ class CompleteViewController: UIViewController {
     //스토리보드 연결
     
     //모달 닫기 버튼
-    @IBOutlet weak var exitButton: UIImageView!
+    @IBOutlet weak var exitModalButton: UIButton!
     //상세 데이터 백그라운드 뷰
-    @IBOutlet weak var dataBackground: UIView!
+    @IBOutlet weak var dataView: UIView!
+    
     
     //~~동안 산소로 바꾸는 양
-    @IBOutlet weak var oxygenChangeAmount: UILabel!
+    @IBOutlet weak var changeToOxyLabel: UILabel!
     //탄소 배출 저감량
-    @IBOutlet weak var reducedCarbon: UILabel!
+    @IBOutlet weak var reducedCarbonLabel: UILabel!
     //오늘 총 저감량
-    @IBOutlet weak var todayReducedCarbon: UILabel!
+    @IBOutlet weak var todayReducedCarbonLabel: UILabel!
     //이동거리
-    @IBOutlet weak var moveDistance: UILabel!
+    @IBOutlet weak var moveDistanceLabel: UILabel!
     //소요시간
-    @IBOutlet weak var timeDuration: UILabel!
+    @IBOutlet weak var timeDurationLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        rectangleCustom()
+        exitButtonCustom()
+    }
+    
+    //dataView 커스텀
+    func rectangleCustom() {
         //모서리 radius 20
-        dataBackground.layer.cornerRadius = 20
+        dataView.layer.cornerRadius = 20
         //그림자 구현
-        dataBackground.layer.shadowOffset = CGSize(width: 0, height: 2)
-        dataBackground.layer.shadowOpacity = 0.25
-        dataBackground.layer.cornerRadius = 15
-        
+        dataView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        dataView.layer.shadowOpacity = 0.25
+        dataView.layer.cornerRadius = 15
+    }
+    
+    //exitModalButton커스텀
+    func exitButtonCustom() {
+        //버튼 사이즈 조절 필요 44*44pt
     }
 }
