0efb780dcb2e1fdd8b31b123a8980d9e0a59f93c
Terry-Koo <xpflxhfl94@naver.com>
2022-07-27 16:52:00 +0900

    [feat] #64 통계 셀 추가

---
+++ Bingha/Bingha/Views/Stat/Cells/StatisticsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticsCollectionViewCell.swift
+//  Bingha
+//
+//  Created by Terry Koo on 2022/07/27.
+//
+
+import UIKit
+
+class StatisticsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
