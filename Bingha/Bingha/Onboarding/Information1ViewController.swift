755a94aeb981bbd07de65bfd4668057f1385a22b
Terry-Koo <xpflxhfl94@naver.com>
2022-07-28 01:51:52 +0900

    [Feat] #66 시작 화면 온보딩 페이지로 변경 및 첫 번째 온보딩 페이지 추가

---
+++ Bingha/Bingha/Onboarding/Information1ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Information1ViewController.swift
+//  Bingha
+//
+//  Created by Terry Koo on 2022/07/28.
+//
+
+import UIKit
+import Lottie
+
+class Information1ViewController: UIViewController {
+    
+}
