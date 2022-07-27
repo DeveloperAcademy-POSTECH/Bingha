107e5e20274d66378b052d48f573753c28ca9792
Terry-Koo <xpflxhfl94@naver.com>
2022-07-27 16:47:05 +0900

    [feat] #64 collectionView 추가

--- Bingha/Bingha/Views/Stat/StatViewController.swift
+++ Bingha/Bingha/Views/Stat/StatViewController.swift
@@ -2,7 +2,22 @@
 //  StatViewController.swift
 //  Bingha
 //
-//  Created by Terry Koo on 2022/07/20.
+//  Created by Terry Koo on 2022/07/27.
 //
 
-import Foundation
+import UIKit
+
+class StatViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = UICollectionViewCell()
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
