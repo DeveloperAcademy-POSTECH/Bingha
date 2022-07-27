a6e142d2b108e8ee2f305aebc8cbdb01e433a17f
Terry-Koo <xpflxhfl94@naver.com>
2022-07-27 16:56:58 +0900

    [feat] #64 Statistics모델 구현

---
+++ Bingha/Bingha/Model/StatisticsModel.swift
@@ -0,0 +1,15 @@
+//
+//  StatisticsModel.swift
+//  Bingha
+//
+//  Created by Terry Koo on 2022/07/27.
+//
+
+import Foundation
+
+struct Statistics {
+    let reducedCarbon: String
+    let walkingDistance: String
+    let walkingTime: String
+    let baseDate: String
+}
