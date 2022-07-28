b7e6575e88d675cf470aee1a3e964d581ce780ae
Terry-Koo <xpflxhfl94@naver.com>
2022-07-28 15:52:22 +0900

    [Feat] #66 앱 처음 구동 시 시작화면 분기

---
+++ Bingha/Bingha/Onboarding/Information2ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Information2ViewController.swift
+//  Bingha
+//
+//  Created by Terry Koo on 2022/07/28.
+//
+
+import UIKit
+
+class Information2ViewController: UIViewController {
+    
+}
---
+++ Bingha/Bingha/Onboarding/Information3ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Information3ViewController.swift
+//  Bingha
+//
+//  Created by Terry Koo on 2022/07/28.
+//
+
+import UIKit
+
+class Information3ViewController: UIViewController {
+    
+}
