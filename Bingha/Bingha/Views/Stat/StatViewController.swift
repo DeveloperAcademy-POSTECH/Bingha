21b08a5a8f3a6f7b1bd3c41a08cea3810c4ba6f5
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-07-29 22:43:27 +0900

    [feat] #52 통계 뷰 각 섹션 컨텐츠 연결

--- Bingha/Bingha/Views/Stat/StatViewController.swift
+++ Bingha/Bingha/Views/Stat/StatViewController.swift
@@ -12,6 +12,8 @@ class StatViewController: UIViewController {
     
     let compareViewModel: CompareViewModel = CompareViewModel()
     let statisticsViewModel: StatisticsViewModel = StatisticsViewModel()
+    let segmentID: String = "CollectionViewSegmentControl"
+    let reducedID: String = "CollectionViewReducedCarbon"
     let compareID: String = "CompareCollectionViewCell"
     let statisticsID: String = "StatisticsCollectionViewCell"
     
@@ -37,9 +39,9 @@ extension StatViewController: UICollectionViewDataSource, UICollectionViewDelega
         case 0: return 1
             //해당 기간 탄소 절감량 섹션
         case 1: return 1
-            //비교 섹션
+            //비교 섹션: 3개. 나중에 추가될수도 있으니까 이거로
         case 2: return compareViewModel.countOfCompareList
-            //걷기 모음 섹션
+            //걷기 모음 섹션: 걷기 횟수만큼
         case 3: return statisticsViewModel.countOfStatisticsList
         default:
             return 0
@@ -48,13 +50,25 @@ extension StatViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     //컬랙션뷰 섹션별로 셀 가져와서 업데이트
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        switch indexPath.item {
+        switch indexPath.section {
+        // 세그먼트 컨트롤. 태그번호로 가져오고 첫번째 세그먼트 기본으로.
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: segmentID, for: indexPath)
+            if let segment = cell.viewWithTag(0) as? UISegmentedControl {
+                segment.selectedSegmentIndex = 0
+            }
+            return cell
+        //탄소저감량 섹션
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reducedID, for: indexPath)
+            return cell
+        //비교 섹션
         case 2:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: compareID, for: indexPath) as! CompareCollectionViewCell
             let compareInfo = compareViewModel.CompareModelInfo(at: indexPath.item)
             cell.update(info: compareInfo)
             return cell
+        //걷기 모음 섹션
         case 3:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: statisticsID, for: indexPath) as! StatisticsCollectionViewCell
             let statisticInfo = statisticsViewModel.statisticsInfo(at: indexPath.item)
@@ -70,7 +84,7 @@ extension StatViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     //컬랙션뷰 간 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        if section == 1 || section == 2 {
+        if section == 1 || section == 2 || section == 3{
             return CGSize(width: collectionView.bounds.width, height: 50)
         }
         return CGSize()
@@ -78,14 +92,25 @@ extension StatViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     //컬랙션뷰 셀 사이 거리
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemSpacing: CGFloat = 10
-        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2.2
-        return CGSize(width: width, height: width)
+        switch indexPath.section {
+        case 2:
+            let itemSpacing: CGFloat = 14
+            let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2.25
+            return CGSize(width: width, height: width)
+        case 3:
+            let itemSpacing: CGFloat = 14
+            let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2.25
+            return CGSize(width: width, height: width)
+        default:
+            let itemSpacing: CGFloat = 14
+            let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2.25
+            return CGSize(width: width, height: width)
+        }
     }
     
     //컬랙션뷰 내부 인셋
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 14, left: 14, bottom: 14, right: 14)
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     }
 }
 
