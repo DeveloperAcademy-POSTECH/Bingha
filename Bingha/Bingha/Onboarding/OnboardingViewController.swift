54603df413918a28a9c800e9cb8bc33e4c12cf24
ryu <munam4@naver.com>
2022-08-01 01:03:16 +0900

    [Feat] #88 온보딩 연결 완

--- Bingha/Bingha/Onboarding/OnboardingViewController.swift
+++ Bingha/Bingha/Onboarding/OnboardingViewController.swift
@@ -14,10 +14,10 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var skipButton: UIButton!
     
     let pages: [OnboardingDataModel] = [
-        OnboardingDataModel(image: #imageLiteral(resourceName: "OnboardingEarth"), title: "걸어서 지구를 살린다고?", description: "차를 타는 대신 걷는 것 만으로도\n탄소배출을 줄여\n지구의 수명을 늘릴 수 있어요."),
-        OnboardingDataModel(image: #imageLiteral(resourceName: "OnboardingIceberg"), title: "매년 녹고 있는 빙하", description: "남은 지구의 온도는 단 1.5도!\n지구 온난화로 인해\n매년 한국 크기만한 빙하가 녹고 있어요."),
-        OnboardingDataModel(image: #imageLiteral(resourceName: "OnboardingCar"), title: "탄소 배출 줄이기 함께해요", description: "차로 출퇴근하는 직장인이 배출하는 탄소는\n하루 평균 7.2kg.\n이는 한 그루의 소나무가\n하루 동안 산소로 바꿀 수 있는 양이에요."),
-        OnboardingDataModel(image: #imageLiteral(resourceName: "OnboardingSimulator"), title: "시작해볼까요", description: "당신의 선택으로 지구를 살릴 수 있어요.\n오늘은 차 대신 걸어보는 건 어떨까요?")
+        OnboardingDataModel(image: #imageLiteral(resourceName: "OnBoardingFourth"), title: "걸어서 지구를 살린다고?", description: "차를 타는 대신 걷는 것 만으로도\n탄소배출을 줄여\n지구의 수명을 늘릴 수 있어요."),
+        OnboardingDataModel(image: #imageLiteral(resourceName: "OnBoardingSecond"), title: "매년 녹고 있는 빙하", description: "남은 지구의 온도는 단 1.5도!\n지구 온난화로 인해\n매년 한국 크기만한 빙하가 녹고 있어요."),
+        OnboardingDataModel(image: #imageLiteral(resourceName: "OnBoardingFirst"), title: "탄소 배출 줄이기 함께해요", description: "차로 출퇴근하는 직장인이 배출하는 탄소는\n하루 평균 7.2kg.\n이는 한 그루의 소나무가\n하루 동안 산소로 바꿀 수 있는 양이에요."),
+        OnboardingDataModel(image: #imageLiteral(resourceName: "OnBoardingThird"), title: "시작해볼까요", description: "당신의 선택으로 지구를 살릴 수 있어요.\n오늘은 차 대신 걸어보는 건 어떨까요?")
     ]
     var currentPage = 0 {
         didSet {
