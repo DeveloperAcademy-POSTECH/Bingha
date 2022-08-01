230943ba9994f7310d20f89456b4046b661c5050
Terry-Koo <xpflxhfl94@naver.com>
2022-08-02 01:36:20 +0900

    [Feat] #117 한번도 안걸었을때는 기본값 0일로 보이게 적용

--- Bingha/Bingha/Views/ReducedCarbonCalculator.swift
+++ Bingha/Bingha/Views/ReducedCarbonCalculator.swift
@@ -10,6 +10,7 @@ import Foundation
 internal class ReducedCarbonCalculator {
     public static let shared: ReducedCarbonCalculator = ReducedCarbonCalculator()
 
+    private init(){}
     
     // 내가 걸은 거리만큼 계산했을 때 탄소 배출이 얼마나 감소했을까
     func reducedCarbon(km distance: Double) -> String {
@@ -17,14 +18,15 @@ internal class ReducedCarbonCalculator {
         return (distance * 134).formatterWithSeparator + "g"
     }
     
-    func reducedCarbonDouble(km distance: Double) -> Double {
-        return (distance * 134)
-    }
-    
     // 내가 걸은 거리만큼 계산했을 때 빙하가 얼마나 지켜졌을까
     func protectedGlacier(km distance: Double) -> String {
         // 자가용으로 1km 이동 시 약 2kg의 빙하가 소멸 -> 1km를 걸어갔을 경우 2kg의 빙하를 지킴
         return (distance * 2).formatterWithSeparator + "kg"
+    }
+    
+
+    func reducedCarbonDouble(km distance: Double) -> Double {
+        return (distance * 134)
     }
     
     // 30년 소나무 1년에 6.6kg의 탄소를 흡수함
@@ -37,6 +39,11 @@ internal class ReducedCarbonCalculator {
     
     // 30년 소나무 1년에 6.6kg의 탄소를 흡수함
     func reducedCarbonToTreeForStat(km distance: Double) -> String {
+        if distance == 0.0 {
+            return "0일"
+        } else if distance < 1.0 {
+            return "약 1일"
+        }
         return String(Int(distance*7)) + "일"
     }
 
@@ -44,6 +51,5 @@ internal class ReducedCarbonCalculator {
     func reducedCarbonToSeaweed(km distance: Double) -> String {
         return (distance*13.4).formatterWithSeparator + "kg"
     }
-
 }
 
